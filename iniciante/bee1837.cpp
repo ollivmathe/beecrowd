76e0cd6901b6755e0aa2283d47246447b13f09df
ollivmathe <ollivmathe@gmail.com>
2026-05-10 13:03:25 -0300

    beecrowd: resolve bee1837

---
+++ iniciante/bee1837.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b, q, r;
+    cin >> a >> b;
+
+    q = a / b;
+    r = a % b;
+
+    if (r < 0) {
+        if (b > 0) {
+            q -= 1;
+            r += b;
+        } else {
+            q += 1;
+            r -= b;
+        }
+    }
+
+    cout << q << " " << r << endl;
+
+    return 0;
+}
