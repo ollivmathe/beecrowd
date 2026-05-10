3109a3f1e0da26806afb9d19cff44c5c3eecd97f
ollivmathe <ollivmathe@gmail.com>
2026-05-10 12:44:28 -0300

    beecrowd: resolve bee1189

---
+++ iniciante/bee1189.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    const int SIZE = 12;
+    int C = 0;
+    double T, R = 0;
+    char O;
+    cin >> O;
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cin >> T;
+
+            if (j < i && j < (SIZE - 1 - i)) {
+                R += T;
+                C++;
+            }
+        }
+    }
+
+    cout << fixed << setprecision(1);
+
+    if (O == 'S') {
+        cout << R << endl;
+    } else {
+        cout << R / C << endl;
+    }
+
+    return 0;
+}
