4e87d0072e4772202d8e514f27d239d665056d02
ollivmathe <ollivmathe@gmail.com>
2026-07-22 21:19:09 -0300

    beecrowd: resolve bee1924

---
+++ iniciante/bee1924.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bee1924 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i <= n; i++) {
+            String course = scanner.nextLine();
+        }
+        System.out.println("Ciencia da Computacao");
+        scanner.close();
+    }
+}
